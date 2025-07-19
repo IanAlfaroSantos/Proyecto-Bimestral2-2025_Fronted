f1a1f3f1738fd83218f274d267ba1bd025849fdb
IanAlfaroSantos <ialfasa2020@gmail.com>
2025-07-18 23:30:45 -0600

    Despliegue

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://127.0.0.1:3000/hoteles/',
+    baseURL: 'https://proyecto-bimestral2-2025-production.up.railway.app/hoteles/',
     timeout: 5000
 })
 
